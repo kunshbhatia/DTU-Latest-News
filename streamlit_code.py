b4e5b96aaf77943adfc6e50a23b259288f09a4e6
Kunsh Bhatia <kunshcomp@gmail.com>
2025-09-08 19:24:54 +0530

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -48,7 +48,7 @@ df.drop_duplicates()
 
 if search_bar:
     search_bar_set = []
-    search_bar_set.append(search_bar.split().lower())
+    search_bar_set.append(search_bar.lower().split())
     text = set(search_bar_set[0])
     req = []
     for i in df['Title'].str.lower().split():
